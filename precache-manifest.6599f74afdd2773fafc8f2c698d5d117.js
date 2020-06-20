b0d00dbfe7eff2ecd58b02495e1ded9f57a6acd0
jennmez <jennmez@gmail.com>
2020-06-20 13:02:26 -0500

    Updates

---
+++ precache-manifest.6599f74afdd2773fafc8f2c698d5d117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2470cbe8b38cd65057990bb7eedc605",
+    "url": "/make-me-food/index.html"
+  },
+  {
+    "revision": "dd216d4f1397d06d37ee",
+    "url": "/make-me-food/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "2f62e49cc71e816ecdf3",
+    "url": "/make-me-food/static/js/2.ae0adbed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/make-me-food/static/js/2.ae0adbed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd216d4f1397d06d37ee",
+    "url": "/make-me-food/static/js/main.d087c939.chunk.js"
+  },
+  {
+    "revision": "5117df12345a12b711a0",
+    "url": "/make-me-food/static/js/runtime-main.7544fad1.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/make-me-food/static/media/mintbean.cf18b629.png"
+  }
+]);
